bc58c933225cb0069f243703a309e2179ed504a7
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-02-09 16:33:50 +0100

    fix: review CSS for the GreenIT metrics

--- src/reporters/readTemplate.js
+++ src/reporters/readTemplate.js
@@ -2,9 +2,9 @@ const folderTemplate = "templates";
 const fs = require("fs");
 const path = require("path");
 const readTemplate = (templateFile) => {
-    const templatePath = path.join(__dirname, folderTemplate, templateFile);
-    return fs.readFileSync(templatePath).toString();
-  };
-module.exports= {
-    readTemplate
+  const templatePath = path.join(__dirname, folderTemplate, templateFile);
+  return fs.readFileSync(templatePath).toString();
+};
+module.exports = {
+  readTemplate,
 };
