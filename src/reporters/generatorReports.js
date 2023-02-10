a4193f631994816dced85fa845e13d52e89bbd53
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-02-10 13:32:30 +0100

    fix: remove unused variable

--- src/reporters/generatorReports.js
+++ src/reporters/generatorReports.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-const folderTemplate = "templates";
 const folderTranslate = "translate";
 const minify = require("html-minifier").minify;
 
